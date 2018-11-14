68be3c9fe6a2a312e9bcd4afd3599451741daebb
Fredy Sanchez <00082817@uca.edu.sv>
2018-11-13 23:13:40 -0600

    Editando app.js

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 
-            function myFunction() {
-                var n00 = document.getElementById("n00").value;
-                var n01 = document.getElementById("n01").value;
-                var n10 = document.getElementById("n10").value;
-                var n11 = document.getElementById("n11").value;
+function myFunction() {
+    var n00 = document.getElementById("n00").value;    
+    var n01 = document.getElementById("n01").value;
+    var n10 = document.getElementById("n10").value;
+    var n11 = document.getElementById("n11").value;
+    let tabla = [n11,n10,n01,n00];
 
-tabla = [n00,n01,n10,n11];
-
-document.getElementById("demo").innerHTML = phi(tabla).toFixed(3);;
-            }
+    let respuesta = document.getElementById("demo");
+    respuesta.textContent = phi(tabla);
+}
 
                   
 
-
-          function phi(table) {
-                return (table[3] * table[0] - table[2] * table[1]) /
-                    Math.sqrt((table[2] + table[3]) *
-                        (table[0] + table[1]) *
-                        (table[1] + table[3]) *
-                        (table[0] + table[2]));
-            }
-
+function phi(table) {
+    return ((table[3] * table[0]) - (table[2] * table[1])) /(
+        Math.sqrt((table[2] + table[3]) *
+                  (table[0] + table[1]) * 
+                  (table[1] + table[3]) *
+                  (table[0] + table[2])));
+}
