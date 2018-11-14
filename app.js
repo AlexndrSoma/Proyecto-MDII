99c1ea4f234b049d8296bb90ceebb4c3f94c8261
Fredy Sanchez <00082817@uca.edu.sv>
2018-11-14 09:14:25 -0600

    Añadiendo tabla

--- app.js
+++ app.js
@@ -1,26 +1,69 @@
+var listado = [];
 
-function myFunction() {
-    let n00 = document.getElementById("n00").value;    
-    let n01 = document.getElementById("n01").value;
-    let n10 = document.getElementById("n10").value;
-    let n11 = document.getElementById("n11").value;
-    let tabla = [n11,n10,n01,n00];
-    console.log(tabla);
-
-    let resultado = document.getElementById('demo');
-
-    let res = phi(tabla);
-    console.log(res);
-    
-    resultado.innerText = res;
-
-    
+function Entrada(estado, desercion) {
+    listado.push({ estado, desercion });
 }
 
-function phi(table) {    
-    return (((table[3] * table[0]) - (table[2] * table[1])) /(
+var tablero = (estado) => {
+    var arrg = [0, 0, 0, 0];
+    for (let i = 0; i < listado.length; i++) {
+        let index = 2;
+        if (listado[i].estado.includes(estado)) index -= 2;
+        if (!listado[i].desercion) index += 1;
+        arrg[index]++;
+    }
+    return arrg;
+}
+
+var boton1 = document.getElementById("boton-llenado");
+
+boton1.onclick = function (evt) {
+    Entrada(["casado"], true);
+
+    Entrada(["NoCasado"], false);
+
+    Entrada(["casado"], false);
+
+    Entrada(["casado"], true);
+
+    Entrada(["NoCasado"], false);
+
+    Entrada(["NoCasado"], true);
+
+    Entrada(["casado"], true);
+
+    Entrada(["NoCasado"], false);
+
+    Entrada(["casado"], true);
+
+    Entrada(["casado"], false);
+
+    Entrada(["casado"], true);
+
+    Entrada(["NoCasado"], false);
+
+    Entrada(["casado"], true);
+
+    Entrada(["casado"], true);
+
+    Entrada(["casado"], true);
+}
+
+function myFunction() {
+    let n00 = parseInt(document.getElementById("n00").value);
+    let n01 = parseInt(document.getElementById("n01").value);
+    let n10 = parseInt(document.getElementById("n10").value);
+    let n11 = parseInt(document.getElementById("n11").value);
+    let tabla = [n11, n10, n01, n00];
+    let resultado = document.getElementById('demo');
+    let res = phi(tabla).toFixed(3);
+    resultado.innerText = res;
+}
+
+function phi(table) {
+    return (((table[3] * table[0]) - (table[2] * table[1])) / (
         Math.sqrt((table[2] + table[3]) *
-                  (table[0] + table[1]) * 
-                  (table[1] + table[3]) *
-                  (table[0] + table[2]))));
+            (table[0] + table[1]) *
+            (table[1] + table[3]) *
+            (table[0] + table[2]))));
 }
