7305a42e7a45fc1df93eca5d5d69d8c98bf093c3
Fredy Sanchez <00082817@uca.edu.sv>
2018-11-14 07:28:04 -0600

    Arreglando sintaxis y debugeando

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 
 function myFunction() {
-    var n00 = document.getElementById("n00").value;    
-    var n01 = document.getElementById("n01").value;
-    var n10 = document.getElementById("n10").value;
-    var n11 = document.getElementById("n11").value;
+    let n00 = document.getElementById("n00").value;    
+    let n01 = document.getElementById("n01").value;
+    let n10 = document.getElementById("n10").value;
+    let n11 = document.getElementById("n11").value;
     let tabla = [n11,n10,n01,n00];
+    console.log(tabla);
 
-    let respuesta = document.getElementById("demo");
-    respuesta.textContent = phi(tabla);
+    let resultado = document.getElementById('demo');
+
+    let res = phi(tabla);
+    console.log(res);
+    
+    resultado.innerText = res;
+
+    
 }
 
-                  
-
-function phi(table) {
-    return ((table[3] * table[0]) - (table[2] * table[1])) /(
+function phi(table) {    
+    return (((table[3] * table[0]) - (table[2] * table[1])) /(
         Math.sqrt((table[2] + table[3]) *
                   (table[0] + table[1]) * 
                   (table[1] + table[3]) *
-                  (table[0] + table[2])));
+                  (table[0] + table[2]))));
 }
